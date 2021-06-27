febe41b0f47f9b13ff076d02cabe98463b8965c1
Kyle Kumar <61950909+KylebKumar@users.noreply.github.com>
2021-06-27 15:34:42 -0700

    adding IOS app

---
+++ VotingApp/VotingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VotingApp
+//
+//  Created by Kyle Kumar on 6/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

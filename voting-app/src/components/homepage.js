1ecc53109a33c24e294f83ce732da951046e8de5
AdityaPh <adityaphatak234@gmail.com>
2021-06-28 00:13:44 -0700

    Adding Fade Functionality to Carousel

--- voting-app/src/components/homepage.js
+++ voting-app/src/components/homepage.js
@@ -93,41 +93,44 @@ export default function HomePage () {
                </div>
                <br clear="right" />
 
-                <Carousel>
-                    <Carousel.Item interval={1000}>
-                        <img
-                            className="d-block w-100"
-                            src={icon1}
-                            alt="First slide"
-                        />
-                        <Carousel.Caption style={{ color:"black" }}>
-                            <h3>First slide label</h3>
-                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                        <Carousel.Item interval={500}>
+                <div>
+                    <h3 style={{textAlign:"center"}}><em>Check Out Our iOS App that Makes Voting Easier!</em></h3>
+                    <Carousel fade>
+                        <Carousel.Item interval={1000}>
                             <img
                                 className="d-block w-100"
-                                src={icon2}
-                                alt="Second slide"
-                        />
-                        <Carousel.Caption style={{ color:"black" }}>
-                            <h3>Second slide label</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img
-                            className="d-block w-100"
-                            src={icon1}
-                            alt="Third slide"
-                        />
-                        <Carousel.Caption style={{ color:"black" }}>
-                            <h3>Third slide label</h3>
-                            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                </Carousel>
+                                src={icon1}
+                                alt="First slide"
+                            />
+                            <Carousel.Caption style={{ color:"black" }}>
+                                <h3>First slide label</h3>
+                                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                            <Carousel.Item interval={500}>
+                                <img
+                                    className="d-block w-100"
+                                    src={icon2}
+                                    alt="Second slide"
+                            />
+                            <Carousel.Caption style={{ color:"black" }}>
+                                <h3>Second slide label</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src={icon1}
+                                alt="Third slide"
+                            />
+                            <Carousel.Caption style={{ color:"black" }}>
+                                <h3>Third slide label</h3>
+                                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                    </Carousel>
+                </div>
             </div>
     );
 }

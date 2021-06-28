d8c1333fb8271c6b33a3dbaeef850e3789ffb733
Adithya Chandrasekar <21148381+adithyachan@users.noreply.github.com>
2021-06-27 19:13:59 -0700

    Navbar formatting

--- voting-app/src/firebase.js
+++ voting-app/src/firebase.js
@@ -8,8 +8,7 @@ const app = firebase.initializeApp({
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 })
 
 export const auth = app.auth()

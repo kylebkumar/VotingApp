4e7b8e7892de4fdaf71ccb54652105e793d694fe
AdityaPh <adityaphatak234@gmail.com>
2021-06-27 23:00:09 -0700

    Adding a card style layout to homepage

--- voting-app/src/components/homepage.js
+++ voting-app/src/components/homepage.js
@@ -7,40 +7,87 @@ export default function HomePage () {
         return (
             <div>
                 <h1 style={{ textAlign:"center" , padding:"0.5em" }}>Why Vote?</h1>
-               <img src={icon1} alt="None" 
-               width="30%" 
-               height="20%" 
-               style={{ display:"block", 
-               paddingBottom: "0.5em",
-            //    align:"left",
-               float:"left"
-               }}>
-               </img>
-               <h5>Why Cast Ballots?</h5>
-               <p>lorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsum
-               lorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsum
-               lorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsum
-               lorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsum
-               lorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsum
-               </p>
+                <div style={{ paddingLeft:"10%"}}>
+                    <img src={icon1} alt="None" 
+                    width="30%" 
+                    height="20%" 
+                    style={{ display:"block", 
+                    //    align:"left",
+                    float:"left"
+                    }}>
+                    </img>
+               </div>
+               <div style={{ paddingRight:"10%" }}>
+                    <h5>Why Cast Ballots?</h5>
+                    <p>lorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsum
+                    lorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsum
+                    lorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsum
+                    lorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsum
+                    lorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsum
+                    </p>
+               </div>
                <br clear="left" />
-
-               <img src={icon1} alt="None" 
-               width="30%" 
-               height="20%" 
-               style={{ display:"block", 
-               paddingBottom: "0.5em",
+               <div style={{ paddingRight:"10%"}}>
+                <img src={icon1} alt="None" 
+                width="30%" 
+                height="20%" 
+                style={{ display:"block", 
+                paddingBottom: "0.5em",
             //    align:"left",
-               float:"right"
-               }}>
-               </img>
-               <h5>Actual Second Topic</h5>
-               <p>lorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsum
-               lorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsum
-               lorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsum
-               lorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsum
-               lorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsum
-               </p>
+                float:"right"
+                }}>
+                </img>
+               </div>
+                <div style={{ paddingLeft:"10%"}}>
+                <h5>Actual Second Topic</h5>
+                <p>lorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsum
+                lorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsum
+                lorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsum
+                lorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsum
+                lorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsum
+                </p>
+               </div>
+               <br clear="right" />
+               <div style={{ paddingLeft:"10%"}}>
+                    <img src={icon1} alt="None" 
+                    width="30%" 
+                    height="20%" 
+                    style={{ display:"block", 
+                    //    align:"left",
+                    float:"left"
+                    }}>
+                    </img>
+               </div>
+               <div style={{ paddingRight:"10%" }}>
+                    <h5>Actual Third Topic</h5>
+                    <p>lorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsum
+                    lorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsum
+                    lorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsum
+                    lorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsum
+                    lorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsum
+                    </p>
+               </div>
+               <br clear="left" />
+               <div style={{ paddingRight:"10%"}}>
+                <img src={icon1} alt="None" 
+                width="30%" 
+                height="20%" 
+                style={{ display:"block", 
+                paddingBottom: "0.5em",
+            //    align:"left",
+                float:"right"
+                }}>
+                </img>
+               </div>
+                <div style={{ paddingLeft:"10%"}}>
+                <h5>Actual Fourth Topic</h5>
+                <p>lorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsum
+                lorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsum
+                lorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsum
+                lorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsum
+                lorem ipsum dipsumlorem ipsum dipsumlorem ipsum dipsum
+                </p>
+               </div>
                <br clear="right" />
 
             </div>

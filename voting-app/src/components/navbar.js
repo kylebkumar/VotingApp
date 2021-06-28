c5b79ef0774d5e6f811d53485c016c099165403f
Adithya Chandrasekar <21148381+adithyachan@users.noreply.github.com>
2021-06-27 21:16:22 -0700

    Added more NavBar Changes

--- voting-app/src/components/navbar.js
+++ voting-app/src/components/navbar.js
@@ -1,36 +1,33 @@
 import React from "react";
-import { Navbar, Nav, Button, Container, NavDropdown} from 'react-bootstrap';
-import "./navbar.css";
+import { Navbar, Nav, Button, Container, NavDropdown } from 'react-bootstrap';
+//import "./navbar.css";
 import { useAuth } from '../contexts/AuthContext';
 
 
 export default function NavBar(){
     const { currentUser, logout } = useAuth()  
-    const accountText = <t style={{color:"white"}}>Account</t>
 
     const navDropDown = 
-    <NavDropdown className="mr-auto" title= { accountText } id="basic-nav-dropdown">
-        <NavDropdown.Item href="/account">Account Page</NavDropdown.Item>
+    <NavDropdown className="mr-auto" title = "Account" id="basic-nav-dropdown">
+        <NavDropdown.Item className="mr-auto" href="/account">Account Page</NavDropdown.Item>
         <NavDropdown.Divider />
-            <NavDropdown.Item href="/account">{currentUser && currentUser.email}</NavDropdown.Item>
-            <NavDropdown.Item onClick = { logout }>Log Out</NavDropdown.Item>
+            <NavDropdown.Item className="mr-auto">{currentUser && currentUser.email}</NavDropdown.Item>
+            <NavDropdown.Item className="mr-auto" onClick = { logout }>Log Out</NavDropdown.Item>
     </NavDropdown>
         
         return (
-            <Navbar className="navigation" variant="light" expand="lg">
+            <Navbar className="navigation" bg="primary" variant="dark" expand="sm">
                 <Navbar.Brand className="title" href="/">Go Vote!</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                    <Nav className="mr-auto">
-                    <Container className>
-                        <t style={{color:"white", minWidth:"15vh"}}>Voting Information</t>
-                        <t style={{color:"white", minWidth:"10vh"}}>Voting Map</t>
-                        <a style={{minWidth:"10vh"}}href="https://github.com/KylebKumar/VotingApp" target="_blank">Check out our iOS app!</a>
-                        {!currentUser && <Button href="/login" variant="link">Log In</Button>}
-                        {!currentUser && <Button href="/signup" variant="primary">Sign Up</Button>}
-                        {currentUser && navDropDown}
-                    </Container>
+                       <Nav.Link>Voting Information</Nav.Link>
+                       <Nav.Link>Voting Map</Nav.Link>
+                       <Nav.Link href= "https://github.com/KylebKumar/VotingApp" target="_blank">Check out our iOS app!</Nav.Link>
+                       {currentUser && navDropDown}
                    </Nav>
+                    {!currentUser && <Button href="/login" variant="primary">Log In</Button>}
+                    {!currentUser && <Button href="/signup" variant="outline-light">Sign Up</Button>}
                 </Navbar.Collapse>
             </Navbar>
         );

b000555e2c43b66de1baeb2c660a4602b4e9a7f3
Adithya Chandrasekar <21148381+adithyachan@users.noreply.github.com>
2021-06-28 23:04:38 -0700

    Added map to map.js

--- voting-app/src/components/map.js
+++ voting-app/src/components/map.js
@@ -1,9 +1,61 @@
-import React from 'react'
-import Iframe from "react-iframe"
+import React, { useState, useRef, useEffect } from 'react'
+import { Button } from "react-bootstrap"
+import mapboxgl from 'mapbox-gl';
+//import Iframe from "react-iframe"
+
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_API_KEY
+
 export default function Map() {
-    return (
-        <div>
-            <Iframe url="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d25390.087789866648!2d-122.04964104999999!3d37.30079845!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sus!4v1624939507121!5m2!1sen!2sus" width="600" height="450"></Iframe>
-        </div>
-    )
+
+  const [userlat, setLat] = useState()
+  const [userlong, setLong] = useState()
+
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setMapLng] = useState(-70.9);
+  const [lat, setMapLat] = useState(42.35);
+  const [zoom, setMapZoom] = useState(9);
+
+  useEffect(() => {
+      if (map.current) return; // initialize map only once
+      map.current = new mapboxgl.Map({
+        container: mapContainer.current,
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [lng, lat],
+        zoom: zoom
+      });
+    });
+  return (
+      <div>
+        <Button onClick={getLocation()} variant="primary">Get Location</Button>
+        <t>Coords: { userlat }, { userlong }</t>
+        <div width="100%" height="1000px"ref={mapContainer} className="map-container"/>
+      </div>
+  )
+
+  function showLocation(position) {
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+    setLat(latitude)
+    setLong(longitude)
+  }
+
+ function errorHandler(err) {
+    if(err.code == 1) {
+       alert("Error: Access is denied!");
+    } else if( err.code == 2) {
+       alert("Error: Position is unavailable!");
+    }
+ }
+
+ function getLocation() {
+
+    if(navigator.geolocation) {
+       // timeout at 60000 milliseconds (60 seconds)
+       var options = {timeout:60000};
+       navigator.geolocation.getCurrentPosition(showLocation, errorHandler, options);
+    } else {
+       alert("Sorry, browser does not support geolocation!");
+    }
+ }
 }

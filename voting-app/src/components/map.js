3a7c045f0c661d793c08defbac520db29420b846
AdityaPh <adityaphatak234@gmail.com>
2021-06-28 19:19:29 -0700

    Revert "Adding Google Maps API"
    This reverts commit a1fbc14721ecf0690befef1e0b1ceb0992e88cf6.

--- voting-app/src/components/map.js
+++ voting-app/src/components/map.js
@@ -1,35 +1,9 @@
-import React, { Component } from 'react';
-import GoogleMapReact from 'google-map-react';
+import React from 'react'
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
-
-class Map extends Component {
-  static defaultProps = {
-    center: {
-        lat:37.7749,
-        lng:-122.4194
-    },
-    zoom: 11
-  };
-
-  render() {
+export default function Map() {
     return (
-      // Important! Always set the container height explicitly
-      <div style={{ height: '100vh', width: '100%' }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: "AIzaSyAl1k1oyCgRPB8jOuB_GmABv1EBYEofAG4" }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
-        >
-          <AnyReactComponent
-            lat={37.7749}
-            lng={-122.4194}
-            text="My Marker"
-          />
-        </GoogleMapReact>
-      </div>
-    );
-  }
+        <div>
+            map
+        </div>
+    )
 }
-
-export default Map;

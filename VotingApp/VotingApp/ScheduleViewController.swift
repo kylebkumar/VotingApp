263d61279df975c8a03dba5ad3583963e6c00511
Kyle Kumar <61950909+KylebKumar@users.noreply.github.com>
2021-06-28 22:08:36 -0700

    adding TabViewController

---
+++ VotingApp/VotingApp/ScheduleViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ScheduleViewController.swift
+//  VotingApp
+//
+//  Created by Kyle Kumar on 6/28/21.
+//
+
+import UIKit
+
+class ScheduleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

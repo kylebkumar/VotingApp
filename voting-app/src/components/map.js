ee93288c4ef00af8b9755426d8be31465c79460e
Adithya Chandrasekar <21148381+adithyachan@users.noreply.github.com>
2021-06-28 21:09:18 -0700

    react-iframe map

--- voting-app/src/components/map.js
+++ voting-app/src/components/map.js
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import Iframe from "react-iframe"
 export default function Map() {
     return (
         <div>
-            map
+            <Iframe url="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d25390.087789866648!2d-122.04964104999999!3d37.30079845!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sus!4v1624939507121!5m2!1sen!2sus" width="600" height="450"></Iframe>
         </div>
     )
 }

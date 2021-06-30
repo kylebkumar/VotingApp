e47930922e834b39a88b9b116de5d800bf19ef24
Kyle Kumar <61950909+KylebKumar@users.noreply.github.com>
2021-06-30 12:37:02 -0700

    Adding UI for Scheduling Appointments

--- VotingApp/VotingApp/ScheduleViewController.swift
+++ VotingApp/VotingApp/ScheduleViewController.swift
@@ -7,9 +7,10 @@
 
 import UIKit
 
-class ScheduleViewController: UIViewController {
+class ScheduleViewController: UIViewController{
 
     var email: String?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,15 +20,5 @@ class ScheduleViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }

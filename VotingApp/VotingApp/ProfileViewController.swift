1dce70ad793ab5b5aac5ca02aca483db056db87d
Kyle Kumar <61950909+KylebKumar@users.noreply.github.com>
2021-06-29 22:31:38 -0700

    adding comments to a rather complex chunk of code

--- VotingApp/VotingApp/ProfileViewController.swift
+++ VotingApp/VotingApp/ProfileViewController.swift
@@ -21,14 +21,20 @@ class ProfileViewController: UIViewController {
     }
     
     override func viewWillAppear(_ animated: Bool) {
+        //NOTE: This entire code segment will likely break if the user does not have data assosiated with their account
         let ref = Database.database().reference()
         
         emailTextView.text = email!
+        //converting the email to a username (swap the '.' with a '-' to make Firebase happy)
         let username = email!.replacingOccurrences(of: ".", with: "-")
         
+        //NOTE: see Realtime Database on Firebase to see how the data is formatted on their end
         ref.child(username).observeSingleEvent(of: .value) { (snapshot) in
+            //get the data corresponding to the email of the user
             let userData = snapshot.value as! [String:Any]
+            //cast the user's data as a dictionary (hard-coded to get their first appointment)
             let appointment1 = userData["Appointment1"] as! Dictionary<String, String>
+            //cast the values in the dictionary into strings and put them in the text-views
             self.locationTextView.text = appointment1["location"] as! String
             self.dateTextView.text = appointment1["date"] as! String
         }
